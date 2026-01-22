028c8843bbff4ee13a587f874296fa2dcf6bd434
Gerald-05gtr <gerald.musiyazwiriyo@student.wsb.edu.pl>
2026-01-22 11:35:56 +0100

    Add Stage 1 exercise S1_SW_01 - Day name

---
+++ stage1/S1_SW_01.js
@@ -0,0 +1,18 @@
+function dayName(n) {
+    switch(n) {
+      case 1: return "Monday";
+      case 2: return "Tuesday";
+      case 3: return "Wednesday";
+      case 4: return "Thursday";
+      case 5: return "Friday";
+      case 6: return "Saturday";
+      case 7: return "Sunday";
+      default: return null; // invalid number
+    }
+  }
+  
+  // Test cases
+  for (let i = 0; i <= 8; i++) {
+    console.log(i, "=>", dayName(i));
+  }
+  
