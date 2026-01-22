eac7cb5eb729fd8231f943b194de406a6cb712b6
Gerald-05gtr <gerald.musiyazwiriyo@student.wsb.edu.pl>
2026-01-22 11:27:07 +0100

    Add Stage 1 exercise S1_VAR_09

---
+++ stage1/S1_VAR_09.js
@@ -0,0 +1,6 @@
+// typeof on an undeclared variable
+console.log("typeof someUndeclaredName =>", typeof someUndeclaredName);
+
+// Explanation:
+// In JavaScript, typeof on a variable that doesn't exist
+// does not throw an error, it returns "undefined".
